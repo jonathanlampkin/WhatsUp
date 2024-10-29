9be702bcdb528c8dbb137e7e511f9654d4863584
Jonathan Lampkin <jmlampkin@gmail.com>
2024-10-29 04:38:40 -0700

    removed procfile, kept heroku.yml file for docker deployment... set port for app.py to 5000

--- main/app.py
+++ main/app.py
@@ -110,4 +110,5 @@ def get_nearby_places():
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     logging.debug("Starting Flask application.")
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
